f7328b2848647965a3358c8a590088db77a58035
Shayaree02 <shm.avalgate@gmail.com>
2025-01-14 16:14:13 +0530

    js first add

--- script.js
+++ script.js
@@ -29,10 +29,12 @@ videocon.addEventListener("mousemove", function(dets){
 }
 videoconAnimation()
 
-gsap.from("#page1 h1", {
-    y:30,
-    opacity:0,
-    delay:0.5,
-    duration:0.5,
-    stagger:0.2
-})
+function loadinganimation(){
+    gsap.from("#page1 h1", {
+        y:100,
+        opacity:0,
+        delay:0.5,
+        duration:0.9,
+        stagger:0.3
+    })
+}
