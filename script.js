02795cde4debdb06b0f5bb3e0f2c801beacbe500
Shayaree02 <shm.avalgate@gmail.com>
2025-01-14 16:49:37 +0530

    img add

--- script.js
+++ script.js
@@ -37,4 +37,11 @@ function loadinganimation(){
         duration:0.9,
         stagger:0.3
     })
+    gsap.from("#page1 #video-container", {
+        scale:0.9,
+        opacity:0,
+        delay:1.3,
+        duration:0.3,
+    })
 }
+loadinganimation()
