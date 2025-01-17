9da1217c28aea1bd35b8e1e511942d5277eb2837
Shayaree02 <shm.avalgate@gmail.com>
2025-01-17 19:24:13 +0530

    navbar

--- script.js
+++ script.js
@@ -63,3 +63,38 @@ function loadinganimation(){
 loadinganimation()
 
 //
+
+document.addEventListener("mousemove", function(dets){
+    gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y
+    })
+})
+
+// for the Single one img (id)
+// document.querySelector("#child1").addEventListener("mouseenter", function(){
+//     gsap.to("#cursor",{
+//         transform: 'translate(-50%, -50%) scale(1)'
+//         })
+// }) 
+
+// document.querySelector("#child1").addEventListener("mouseleave", function(){
+//     gsap.to("#cursor",{
+//         transform: 'translate(-50%, -50%) scale(0)'
+//         })
+// }) 
+
+document.querySelectorAll(".child").forEach(function(elem){
+
+        elem.addEventListener("mouseenter",function(){
+            gsap.to("#cursor",{
+                transform: 'translate(-50%, -50%) scale(1)'
+            })
+        })
+        elem.addEventListener("mouseleave",function(){
+            gsap.to("#cursor",{
+                transform: 'translate(-50%, -50%) scale(0)'
+            })
+        })
+})
+
