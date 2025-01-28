b2942c85451e6394ff5bada594efee96355b912a
JNC <jit.chandra7@gmail.com>
2025-01-28 16:11:48 +0530

    test3

--- script.js
+++ script.js
@@ -174,6 +174,49 @@ function cursorAnimation(){
 }
 cursorAnimation();
 
+function cursorAnimationProduct(){
+    document.addEventListener("mousemove", function(dets){
+        gsap.to("#cursor",{
+            left:dets.x,
+            top:dets.y
+        });
+    });
+    
+    // for the Single one img (id)
+    // document.querySelector("#child1").addEventListener("mouseenter", function(){
+    //     gsap.to("#cursor",{
+    //         transform: 'translate(-50%, -50%) scale(1)'
+    //         })
+    // }) 
+    
+    // document.querySelector("#child1").addEventListener("mouseleave", function(){
+    //     gsap.to("#cursor",{
+    //         transform: 'translate(-50%, -50%) scale(0)'
+    //         })
+    // }) 
+    
+    document.querySelectorAll(".product").forEach(function(elem){
+    
+            elem.addEventListener("mouseenter",function(){
+                gsap.to("#cursor",{
+                    transform: 'translate(-50%, -50%) scale(1)'
+                });
+            });
+            elem.addEventListener("mouseleave",function(){
+                gsap.to("#cursor",{
+                    transform: 'translate(-50%, -50%) scale(0)'
+                });
+            });
+    });
+
+}
+cursorAnimationProduct();
+
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
 
 
 // 1:10:25min 
